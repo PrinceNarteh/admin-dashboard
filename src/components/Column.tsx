47374018714b4aa492a522cb2e2b0d6a126cda63
Prince Narteh <prinarteh@gmail.com>
2022-04-05 07:09:49 +0000

    adds Column component

---
+++ src/components/Column.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+interface ColumnProps {
+  sm?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  md?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  lg?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+}
+
+export const Column = styled.div<ColumnProps>``;
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -2,4 +2,6 @@ import styled from "styled-components";
 
 export const Row = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  margin: 0 -1.5rem;
 `;
